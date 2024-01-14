57abcfc88a3ca828f1de83f5a81eb00da92f6cc2
sladek <sladek.miro@gmail.com>
2024-01-15 00:09:22 +0100

    Added log from resulting model

--- parallel-test/myTest.R
+++ parallel-test/myTest.R
@@ -11,9 +11,9 @@ cluster2 <- rep('192.168.2.14', times=4L)
 cluster <- c(cluster1, cluster2)
 
 cl <- parallelly::makeClusterPSOCK(cluster, user='sladekm',
-      outfile='/tmp/mytest.log',
-      default_packages=c("stringr", "stats", "logger"),
-      verbose=TRUE)
+                                   outfile='/tmp/mytest.log',
+                                   default_packages=c("stringr", "stats", "logger"),
+                                   verbose=TRUE)
 print(cl)
 # end of start of the cluster 
 
@@ -27,19 +27,18 @@ myTestFunction <- function(nRepeate, parameter, FUN) {
   for (i in 1:nRepeate) {
     res <- FUN(parameter)
   }
-#  as.character(cat(parameter,'\n', sep = ''))
+  res
 }
 
 source('bindToEnv.R') # Download from: http://winvector.github.io/Parallel/bindToEnv.R
 # We will build the single argument function we are going to pass to parallel
 mkWorker <- function() {
-# Bind all the necessary variables and function to the environment so that remote node can see it
-    bindToEnv(objNames=c('parallelInput', 'myTestFunction', 'logElapsedTime'))
-# We will return function that will execute myTestFunction() on the node
+  # Bind all the necessary variables and function to the environment so that remote node can see it
+  bindToEnv(objNames=c('parallelInput', 'myTestFunction', 'logElapsedTime'))
+  # We will return function that will execute myTestFunction() on the node
   function(parallelInput){
     currentTs <- Sys.time()
     myTestFunction(10, parallelInput, log)
-#    logElapsedTime(currentTs)
   }
 }
 
@@ -50,13 +49,13 @@ logElapsedTime <- function(previousTimestamp) {
   log_message <- cat( '[PID=',pid,  ', hostname: ', system('hostname', intern = T) , ', elapsed time: ', elapsed, ' seconds]\n',  sep='')
 }
 
-myTestFunction(4, 100, sqrt)
-
 currentTs <- Sys.time()
 # Parallel execution
-parallelInput <- seq(1, 12000000)
+parallelInput <- seq(1, 120)
 models <- parallel::parLapply(cl, parallelInput, mkWorker())
 # End of parallel execution
+
+print(models)
 
 logElapsedTime(currentTs)
 
